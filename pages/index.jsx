c9c13d340c708558b4b7b0fe011408435fce8359
renzynx <magicdorm@hotmail.com>
2021-12-21 09:23:20 +0700

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,31 +5,7 @@ import { Navbar } from "../components/Navbar";
 export default function Home({ track_list }) {
   return (
     <>
-      <Head>
-        <title>Lyric Finder</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="google-site-verification"
-          content="t1LgsLYBK9cGjBAM5LO7ivCLSrhw__xvJEBoBsiAK2I"
-        />
-        <meta
-          name="description"
-          content="A website made to find your favourite song lyric. Got a song that stuck on your mind for days and still doesn't the know the song lyric, use lyricfinder to discover lyrics now."
-        />
-        <meta
-          name="keywords"
-          content="lyric finder, lyric find, lyric find xyz, lyricfinder, lyric, lyrics, lyricsfinder, lyricfinder.xyz, renzynx.space, renzynx, find lyric, save your tears, save your tear"
-        />
-        <meta name="robots" content="index,follow" />
-        <meta name="author" content="renzynx, magicdorm@hotmail.com"></meta>
-        <meta name="owner" content="renzynx"></meta>
-        <meta name="identifier" content="https://lyricfinder.xyz"></meta>
-        <meta name="og:title" content="Lyric Finder" />
-        <meta name="og:type" content="music" />
-        <meta name="og:site_name" content="Lyric Finder" />
-      </Head>
-
-      <Navbar />
+            <Navbar />
       <Tracks track_list={track_list} title="THIS WEEK CHART" />
     </>
   );
